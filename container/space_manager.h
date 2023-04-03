248ecbc6fffafb255b46a10648b9c60445e20689
yuyuaqwq <1454832774@qq.com>
2023-04-03 18:12:33 +0800

    调整代码

--- container/space_manager.h
+++ container/space_manager.h
@@ -78,36 +78,49 @@ extern "C" {
     void space_manager_type_name##SpaceManagerFree(space_manager_type_name##SpaceHead* head, id_type list_order, id_type free_offset, id_type count) { \
         id_type cur_offset = head->first_block[list_order]; \
         space_manager_type_name##SpaceBlock* prev_block = (space_manager_type_name##SpaceBlock*)(&head->first_block[list_order]); \
-        \
-        space_manager_type_name##SpaceBlock* cur_block = (space_manager_type_name##SpaceBlock*)(&head->obj_arr[free_offset]); \
+        space_manager_type_name##SpaceBlock* cur_block; \
         /* 尝试合并与当前块连续的前后空闲块 */ \
-        bool prev = false, next = false; \
+        space_manager_type_name##SpaceBlock* free_prev_prev_block = NULL, * free_next_prev_block = NULL; \
+        space_manager_type_name##SpaceBlock* free_prev_block = NULL, * free_next_block = NULL; \
         while (cur_offset != referencer##_InvalidId) { \
-            space_manager_type_name##SpaceBlock* cur_block = (space_manager_type_name##SpaceBlock*)(&head->obj_arr[cur_offset]); \
-            if (!next && free_offset + count == cur_offset) { \
-                /* 找到连续的空闲下一块 */ \
+            cur_block = (space_manager_type_name##SpaceBlock*)(&head->obj_arr[cur_offset]); \
+            if (!free_next_block && free_offset + count == cur_offset) { \
+                /* 找到连续的空闲后继块 */ \
+                if (free_prev_block) { \
+                    /* 前继块需要删除 */ \
+                    free_prev_prev_block->next_block_offset = free_prev_block->next_block_offset; \
+                } \
+                count += cur_block->count; \
+                id_type next_offset = cur_block->next_block_offset; \
+                cur_block = (space_manager_type_name##SpaceBlock*)(&head->obj_arr[free_offset]); \
+                cur_block->count = count; \
+                cur_block->next_block_offset = next_offset; \
+                prev_block->next_block_offset = free_offset; \
+                free_next_prev_block = prev_block; \
+                free_next_block = cur_block; \
+            } \
+            else if (!free_prev_block && cur_offset + cur_block->count == free_offset) { \
+                /* 找到连续的空闲前继块，更新count和free_offset是为了找可能存在的连续的空闲下一块 */ \
+                if (free_next_block) { \
+                    /* 后继块需要删除 */ \
+                    free_next_prev_block->next_block_offset = free_next_block->next_block_offset; \
+                } \
+                free_offset = cur_offset; \
                 count += cur_block->count; \
                 cur_block->count = count; \
-                prev_block->next_block_offset = free_offset; \
-                next = true; \
-            } \
-            else if (!prev && cur_offset + cur_block->count == free_offset) { \
-                /* 找到连续的空闲上一块 */ \
-                free_offset = cur_offset; \
-                count += cur_block->count;\
-                cur_block->count = count; \
-                prev_block->next_block_offset = free_offset; \
-                prev = true; \
+                free_prev_prev_block = prev_block; \
+                free_prev_block = cur_block; \
             } \
             else { \
                 /* 没有合并才更新，找到连续的空闲下一块也不更新这块逻辑比较复杂
                  主要为了使得下一次循环若找到连续的空闲上一块时，prev能够正确的指向合并后的free_block */ \
                 prev_block = cur_block; \
             } \
-            if (prev && next) break; \
+            if (free_prev_block && free_next_block) break; \
             cur_offset = cur_block->next_block_offset; \
         } \
-        if (!prev && !next) { \
+        if (!free_prev_block && !free_next_block) { \
+            cur_block = (space_manager_type_name##SpaceBlock*)(&head->obj_arr[free_offset]); \
             cur_block->next_block_offset = head->first_block[list_order]; \
             cur_block->count = count; \
             head->first_block[list_order] = free_offset; \
