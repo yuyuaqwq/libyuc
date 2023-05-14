8e845b2c5505d2d3b8720443c39b0e9a679ba383
yuyuaqwq <1454832774@qq.com>
2023-05-14 15:44:58 +0800

    对象池抽象优化

--- space_manager/object_pool.h
+++ space_manager/object_pool.h
@@ -35,7 +35,7 @@ extern "C" {
 	obj_type* object_pool_name##ObjectPoolAlloc(object_pool_name##ObjectPool* pool, object_pool_name##ObjectPoolBucketEntry* ret_entry); \
 	void object_pool_name##ObjectPoolFree(object_pool_name##ObjectPool* pool, object_pool_name##ObjectPoolBucketEntry* free_entry); \
 
-#define CUTILS_SPACE_MANAGER_OBJECT_POOL_DEFINE(object_pool_name, obj_type, bucket_id_type, index_id_type, bucket_entry_count, bucket_allocator, bucket_referencer) \
+#define CUTILS_SPACE_MANAGER_OBJECT_POOL_DEFINE(object_pool_name, obj_type, bucket_id_type, index_id_type, bucket_entry_count, bucket_allocator, bucket_referencer, indexer) \
 	CUTILS_CONTAINER_HASH_TABLE_DEFINE(object_pool_name##ObjectPoolBucket, bucket_id_type, bucket_id_type, CUTILS_OBJECT_ALLOCATOR_DEFALUT, CUTILS_CONTAINER_HASH_TABLE_DEFAULT_ACCESSOR, CUTILS_OBJECT_MOVER_DEFALUT, CUTILS_CONTAINER_HASH_TABLE_DEFAULT_HASHER, CUTILS_OBJECT_COMPARER_DEFALUT) \
 	void object_pool_name##ObjectPoolBucketEntryInit(object_pool_name##ObjectPoolBucketEntry* entry) { \
 		entry->bucket_id = bucket_referencer##_InvalidId; \
@@ -55,7 +55,7 @@ extern "C" {
 		object_pool_name##ObjectPoolBucketHashTableInit(&table, 0, 0); \
 		while (bucket_id != bucket_referencer##_InvalidId) { \
 			obj_type* bucket = (obj_type*)bucket_referencer##_Reference(pool, bucket_id); \
-			object_pool_name##ObjectPoolBucketEntry* bucket_entry = (object_pool_name##ObjectPoolBucketEntry*)&bucket[index]; \
+			object_pool_name##ObjectPoolBucketEntry* bucket_entry = (object_pool_name##ObjectPoolBucketEntry*)&indexer##_Get(pool, &bucket, index); \
 			object_pool_name##ObjectPoolBucketHashTablePut(&table, &bucket_id); \
 			bucket_id = bucket_entry->bucket_id; \
 			index = bucket_entry->index; \
@@ -79,11 +79,11 @@ extern "C" {
 			bucket_id_type bucket_id = (bucket_id_type)bucket_allocator##_CreateMultiple(pool, obj_type, bucket_entry_count); \
 			obj_type* bucket = (obj_type*)bucket_referencer##_Reference(pool, bucket_id); \
 			for (ptrdiff_t i = 0; i < bucket_entry_count - 1; i++) { \
-				object_pool_name##ObjectPoolBucketEntry* bucket_entry = (object_pool_name##ObjectPoolBucketEntry*)&bucket[i]; \
+				object_pool_name##ObjectPoolBucketEntry* bucket_entry = (object_pool_name##ObjectPoolBucketEntry*)&indexer##_Get(pool, &bucket, i); \
 				bucket_entry->bucket_id = bucket_id; \
 				bucket_entry->index = i + 1; \
 			} \
-			object_pool_name##ObjectPoolBucketEntry* bucket_entry = (object_pool_name##ObjectPoolBucketEntry*)&bucket[bucket_entry_count - 1]; \
+			object_pool_name##ObjectPoolBucketEntry* bucket_entry = (object_pool_name##ObjectPoolBucketEntry*)&indexer##_Get(pool, &bucket, bucket_entry_count - 1); \
 			bucket_entry->bucket_id = pool->first_entry.bucket_id; \
 			bucket_entry->index = pool->first_entry.index; \
 			\
@@ -94,7 +94,7 @@ extern "C" {
 		ret_entry->bucket_id = pool->first_entry.bucket_id; \
 		ret_entry->index = pool->first_entry.index; \
 		obj_type* bucket = (obj_type*)bucket_referencer##_Reference(pool, ret_entry->bucket_id); \
-		object_pool_name##ObjectPoolBucketEntry* bucket_entry = (object_pool_name##ObjectPoolBucketEntry*)&bucket[ret_entry->index]; \
+		object_pool_name##ObjectPoolBucketEntry* bucket_entry = (object_pool_name##ObjectPoolBucketEntry*)&indexer##_Get(pool, &bucket, ret_entry->index); \
 		pool->first_entry.bucket_id = bucket_entry->bucket_id; \
 		pool->first_entry.index = bucket_entry->index; \
 		bucket_referencer##_Dereference(pool, bucket); \
@@ -102,7 +102,7 @@ extern "C" {
 	} \
 	void object_pool_name##ObjectPoolFree(object_pool_name##ObjectPool* pool, object_pool_name##ObjectPoolBucketEntry* free_entry) { \
 		obj_type* bucket = (obj_type*)bucket_referencer##_Reference(pool, free_entry->bucket_id); \
-		object_pool_name##ObjectPoolBucketEntry* bucket_entry = (object_pool_name##ObjectPoolBucketEntry*)&bucket[free_entry->index]; \
+		object_pool_name##ObjectPoolBucketEntry* bucket_entry = (object_pool_name##ObjectPoolBucketEntry*)&indexer##_Get(pool, &bucket, free_entry->index); \
 		bucket_entry->bucket_id = pool->first_entry.bucket_id; \
 		bucket_entry->index = pool->first_entry.index; \
 		pool->first_entry.bucket_id = free_entry->bucket_id; \
@@ -113,7 +113,6 @@ extern "C" {
 	} \
 
 //CUTILS_SPACE_MANAGER_OBJECT_POOL_DECLARATION(Int, int64_t, void*, int16_t)
-
 //CUTILS_SPACE_MANAGER_OBJECT_POOL_DEFINE(Int, int64_t, IntObjectPoolBucketEntry*, int16_t, 512, CUTILS_OBJECT_ALLOCATOR_DEFALUT, CUTILS_OBJECT_REFERENCER_DEFALUT)
 
 #ifdef __cplusplus
