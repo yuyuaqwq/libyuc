f325e63b68c3bdd85b82476bb22737a27ea405f6
yuyuaqwq <1454832774@qq.com>
2023-10-04 09:59:00 +0800

    readme

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,6 +3,5 @@
 int main(int argc, char** argv) {
     printf("Running main() from %s\n", __FILE__);
     testing::InitGoogleTest(&argc, argv);
-    ::testing::GTEST_FLAG(filter) = "ArTreeTest*";
     return RUN_ALL_TESTS();
 }
